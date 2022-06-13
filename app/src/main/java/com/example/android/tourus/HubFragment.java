f93f8bec31dacccc83f8474f761b615350fa7f40
Mark Benson <mbenson707@gmail.com>
2022-06-13 16:19:38 +1200

    Adding code for Attraction fragment to populate fragment when required cont.

--- app/src/main/java/com/example/android/tourus/HubFragment.java
+++ app/src/main/java/com/example/android/tourus/HubFragment.java
@@ -1,13 +1,22 @@
 package com.example.android.tourus;
 
+import android.app.Notification;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HubFragment extends Fragment {
 
-}
+
+    }
